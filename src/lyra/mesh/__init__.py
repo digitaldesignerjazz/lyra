bbbe168287a6f375288655c9a376143732eb7f6a
digitaldesignerjazz <135103659+digitaldesignerjazz@users.noreply.github.com>
2026-06-09 06:50:45 +0200

    feat(mesh): add mesh package __init__ with exports

---
+++ src/lyra/mesh/__init__.py
@@ -0,0 +1,17 @@
+from .connector import (
+    MeshConfig,
+    MeshConnector,
+    MeshEvent,
+    SimulatedMeshConnector,
+    YggdrasilConnector,
+    get_mesh_connector,
+)
+
+__all__ = [
+    "MeshConfig",
+    "MeshConnector",
+    "MeshEvent",
+    "SimulatedMeshConnector",
+    "YggdrasilConnector",
+    "get_mesh_connector",
+]
